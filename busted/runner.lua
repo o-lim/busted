f60a10670b61161fbea68029e2a85bd7a13b3841
Oscar Lim <olim@ucla.edu>
2015-06-08 11:32:20 -0700

    Refactor runner to conform to style guide

--- busted/runner.lua
+++ busted/runner.lua
@@ -15,9 +15,9 @@ return function(options)
   local busted = require 'busted.core'()
 
   local cli = require 'busted.modules.cli'(options)
-  local filterLoader = require 'busted.modules.filter_loader'()
-  local helperLoader = require 'busted.modules.helper_loader'()
-  local outputHandlerLoader = require 'busted.modules.output_handler_loader'()
+  local filter_loader = require 'busted.modules.filter_loader'()
+  local helper_loader = require 'busted.modules.helper_loader'()
+  local output_handler_loader = require 'busted.modules.output_handler_loader'()
 
   local luacov = require 'busted.modules.luacov'()
 
@@ -26,73 +26,73 @@ return function(options)
   local level = 2
   local info = debug.getinfo(level, 'Sf')
   local source = info.source
-  local fileName = source:sub(1,1) == '@' and source:sub(2) or source
+  local filename = source:sub(1,1) == '@' and source:sub(2) or source
 
   -- Parse the cli arguments
-  local appName = path.basename(fileName)
-  cli:set_name(appName)
-  local cliArgs, err = cli:parse(arg)
-  if not cliArgs then
+  local app_name = path.basename(filename)
+  cli:set_name(app_name)
+  local cliargs, err = cli:parse(arg)
+  if not cliargs then
     io.stderr:write(err .. '\n')
     osexit(1, true)
   end
 
-  if cliArgs.version then
+  if cliargs.version then
     -- Return early if asked for the version
     print(busted.version)
     osexit(0, true)
   end
 
   -- Load current working directory
-  local _, err = path.chdir(path.normpath(cliArgs.directory))
+  local _, err = path.chdir(path.normpath(cliargs.directory))
   if err then
-    io.stderr:write(appName .. ': error: ' .. err .. '\n')
+    io.stderr:write(app_name .. ': error: ' .. err .. '\n')
     osexit(1, true)
   end
 
   -- If coverage arg is passed in, load LuaCovsupport
-  if cliArgs.coverage then
+  if cliargs.coverage then
     luacov()
   end
 
   -- If auto-insulate is disabled, re-register file without insulation
-  if not cliArgs['auto-insulate'] then
+  if not cliargs['auto-insulate'] then
     busted.register('file', 'file', {})
   end
 
   -- If lazy is enabled, make lazy setup/teardown the default
-  if cliArgs.lazy then
+  if cliargs.lazy then
     busted.register('setup', 'lazy_setup')
     busted.register('teardown', 'lazy_teardown')
   end
 
-  -- Add additional package paths based on lpath and cpath cliArgs
-  if #cliArgs.lpath > 0 then
-    package.path = (cliArgs.lpath .. ';' .. package.path):gsub(';;',';')
+  -- Add additional package paths based on lpath and cpath cliargs
+  if #cliargs.lpath > 0 then
+    package.path = (cliargs.lpath .. ';' .. package.path):gsub(';;',';')
   end
 
-  if #cliArgs.cpath > 0 then
-    package.cpath = (cliArgs.cpath .. ';' .. package.cpath):gsub(';;',';')
+  if #cliargs.cpath > 0 then
+    package.cpath = (cliargs.cpath .. ';' .. package.cpath):gsub(';;',';')
   end
 
   -- watch for test errors and failures
   local failures = 0
   local errors = 0
-  local quitOnError = not cliArgs['keep-going']
+  local quit_on_error = not cliargs['keep-going']
 
   busted.subscribe({ 'error', 'output' }, function(element, parent, message)
-    io.stderr:write(appName .. ': error: Cannot load output library: ' .. element.name .. '\n' .. message .. '\n')
+    io.stderr:write(app_name .. ': error: Cannot load output library: ' .. element.name .. '\n' .. message .. '\n')
     return nil, true
   end)
 
   busted.subscribe({ 'error', 'helper' }, function(element, parent, message)
-    io.stderr:write(appName .. ': error: Cannot load helper script: ' .. element.name .. '\n' .. message .. '\n')
+    io.stderr:write(app_name .. ': error: Cannot load helper script: ' .. element.name .. '\n' .. message .. '\n')
     return nil, true
   end)
 
   busted.subscribe({ 'error' }, function(element, parent, message)
     errors = errors + 1
-    busted.skip_all = quitOnError
+    busted.skip_all = quit_on_error
     return nil, true
   end)
 
@@ -102,81 +102,81 @@ return function(options)
     else
       errors = errors + 1
     end
-    busted.skip_all = quitOnError
+    busted.skip_all = quit_on_error
     return nil, true
   end)
 
   -- Set up output handler to listen to events
-  local outputHandlerOptions = {
-    verbose = cliArgs.verbose,
-    suppressPending = cliArgs['suppress-pending'],
-    language = cliArgs.lang,
-    deferPrint = cliArgs['defer-print'],
-    arguments = cliArgs.Xoutput
+  local output_handler_options = {
+    verbose = cliargs.verbose,
+    suppressPending = cliargs['suppress-pending'],
+    language = cliargs.lang,
+    deferPrint = cliargs['defer-print'],
+    arguments = cliargs.Xoutput
   }
 
-  local outputHandler = outputHandlerLoader(busted, cliArgs.output, outputHandlerOptions, options.default_output)
-  outputHandler:subscribe(outputHandlerOptions)
+  local output_handler = output_handler_loader(busted, cliargs.output, output_handler_options, options.default_output)
+  output_handler:subscribe(output_handler_options)
 
-  if cliArgs['enable-sound'] then
-    require 'busted.outputHandlers.sound'(outputHandlerOptions)
+  if cliargs['enable-sound'] then
+    require 'busted.outputHandlers.sound'(output_handler_options)
   end
 
   -- Set up randomization options
-  busted.sort = cliArgs['sort-tests']
-  busted.randomize = cliArgs['shuffle-tests']
-  busted.randomseed = tonumber(cliArgs.seed) or os.time()
+  busted.sort = cliargs['sort-tests']
+  busted.randomize = cliargs['shuffle-tests']
+  busted.randomseed = tonumber(cliargs.seed) or os.time()
 
   -- Set up tag and test filter options
-  local filterLoaderOptions = {
-    tags = cliArgs.tags,
-    exclude_tags = cliArgs['exclude-tags'],
-    filter = cliArgs.filter,
-    filter_out = cliArgs['filter-out'],
-    list = cliArgs.list,
-    nokeepgoing = not cliArgs['keep-going'],
+  local filter_loader_options = {
+    tags = cliargs.tags,
+    exclude_tags = cliargs['exclude-tags'],
+    filter = cliargs.filter,
+    filter_out = cliargs['filter-out'],
+    list = cliargs.list,
+    nokeepgoing = not cliargs['keep-going'],
   }
 
   -- Load tag and test filters
-  filterLoader(busted, filterLoaderOptions)
+  filter_loader(busted, filter_loader_options)
 
   -- Set up helper script
-  if cliArgs.helper and cliArgs.helper ~= '' then
-    local helperOptions = {
-      verbose = cliArgs.verbose,
-      language = cliArgs.lang,
-      arguments = cliArgs.Xhelper
+  if cliargs.helper and cliargs.helper ~= '' then
+    local helper_options = {
+      verbose = cliargs.verbose,
+      language = cliargs.lang,
+      arguments = cliargs.Xhelper
     }
 
-    helperLoader(busted, cliArgs.helper, helperOptions)
+    helper_loader(busted, cliargs.helper, helper_options)
   end
 
   -- Set up test loader options
-  local testFileLoaderOptions = {
-    verbose = cliArgs.verbose,
-    sort = cliArgs['sort-files'],
-    shuffle = cliArgs['shuffle-files'],
-    recursive = cliArgs['recursive'],
+  local test_file_loader_options = {
+    verbose = cliargs.verbose,
+    sort = cliargs['sort-files'],
+    shuffle = cliargs['shuffle-files'],
+    recursive = cliargs['recursive'],
     seed = busted.randomseed
   }
 
   -- Load test directory
-  local rootFiles = cliArgs.ROOT or { fileName }
-  local pattern = cliArgs.pattern
-  local testFileLoader = require 'busted.modules.test_file_loader'(busted, cliArgs.loaders)
-  testFileLoader(rootFiles, pattern, testFileLoaderOptions)
+  local root_files = cliargs.ROOT or { filename }
+  local pattern = cliargs.pattern
+  local test_file_loader = require 'busted.modules.test_file_loader'(busted, cliargs.loaders)
+  test_file_loader(root_files, pattern, test_file_loader_options)
 
   -- If running standalone, setup test file to be compatible with live coding
-  if not cliArgs.ROOT then
+  if not cliargs.ROOT then
     local ctx = busted.context.get()
     local children = busted.context.children(ctx)
     local file = children[#children]
     debug.getmetatable(file.run).__call = info.func
   end
 
-  local runs = cliArgs['repeat']
+  local runs = cliargs['repeat']
   local execute = require 'busted.execute'(busted)
-  execute(runs, { seed = cliArgs.seed })
+  execute(runs, { seed = cliargs.seed })
 
   busted.publish({ 'exit' })
 
